17338eb04506d87eca0faf2d888064c571d8ddc1
Elahe Dastan <elahe.dstn@gmail.com>
2020-04-17 09:06:31 +0430

    feat: Create Store Interface

--- store/store.go
+++ store/store.go
@@ -3,26 +3,62 @@ package store
 import (
 	"database/sql"
 	"fmt"
-	"snapfood/model"
 	"strings"
+
+	"github.com/elahe-dastan/record-appender/model"
 )
 
-func Insert(db *sql.DB, data []model.Data) error {
-	values := ""
-	for _,d := range data {
-		values += "('"
-		values += d.FirstName
-		values += "','"
-		values += d.LastName
-		values += "','"
-		values += d.Number
-		values += "'),"
+type Data interface {
+	Insert(data []model.Data) error
+	Retrieve(limit int, offset int) ([]model.Data, error)
+}
+
+type SQLData struct {
+	DB *sql.DB
+}
+
+func (s SQLData) Retrieve(limit int, offset int) ([]model.Data, error) {
+	rows, err := s.DB.Query("SELECT * from data OFFSET $1 LIMIT 100;", offset, limit)
+	if err != nil {
+		return nil, err
 	}
 
-	values = strings.TrimSuffix(values, ",")
-	fmt.Println("INSERT INTO data (first, last, number) VALUES " + values)
+	if rows.Err() != nil {
+		return nil, err
+	}
 
-	_, err := db.Exec("INSERT INTO data (first, last, number) VALUES " + values + ";")
+	result := make([]model.Data, 0)
+
+	for rows.Next() {
+		var data model.Data
+		if err := rows.Scan(&data.FirstName, &data.LastName, &data.Number); err != nil {
+			return nil, err
+		}
+
+		result = append(result, data)
+	}
+
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (s SQLData) Insert(data []model.Data) error {
+	query := "INSERT INTO data (first, last, number) VALUES "
+	parameters := make([]interface{}, 0)
+
+	for i, d := range data {
+		// nolint: gomnd
+		query += fmt.Sprintf("($%d, $%d, $%d),", 3*i+1, 3*i+2, 3*i+3)
+
+		parameters = append(parameters, d.FirstName, d.LastName, d.Number)
+	}
+
+	query = strings.TrimSuffix(query, ",")
+
+	_, err := s.DB.Exec(query, parameters...)
 
 	return err
 }
