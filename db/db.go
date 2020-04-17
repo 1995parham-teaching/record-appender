4648fa344890500dc7b3b42cb08c8834e6fe641e
Elahe Dastan <elahe.dstn@gmail.com>
2020-04-17 09:05:34 +0430

    feat: Use Database Configuration in DB Module

--- db/db.go
+++ db/db.go
@@ -4,11 +4,12 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/elahe-dastan/record-appender/config"
 	_ "github.com/lib/pq" //adding dialect for postgres
 )
 
-func New() *sql.DB {
-	db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=snapfood password=postgres sslmode=disable")
+func New(database config.Database) *sql.DB {
+	db, err := sql.Open("postgres", database.Cstring())
 	if err != nil {
 		log.Printf("can not open connection to database due to the following err\n: %s", err)
 	}
